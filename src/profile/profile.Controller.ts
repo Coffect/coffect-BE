c069b84b31ab6647118497c279867cba893bd8f8
seoki <seoki180@naver.com>
2025-07-31 13:25:42 +0900

    docs : 키워드설명 추가

--- src/profile/profile.Controller.ts
+++ src/profile/profile.Controller.ts
@@ -39,7 +39,7 @@ export class ProfileController extends Controller {
 
   /**
    * 본인의 기본적인 프로필
-   * 쓰레드수, 팔로잉, 팔로워, 유저정보를 반환한다
+   * 쓰레드수, 팔로잉, 팔로워, 유저정보, 관심키워드를 반환한다
    *
    * @summary 본인 프로필 조회
    *
@@ -188,7 +188,8 @@ export class ProfileController extends Controller {
   }
   /**
    * 유저의 프로필을 조회한다
-   * 조회하려는 유저의 아이디를 검색한다
+   * 조회하려는 유저의 아이디를 검색한다,
+   * 쓰레드수, 팔로잉, 팔로워, 유저정보, 관심키워드를 반환한다
    *
    * @summary 유저 프로필 조회
    * @param id 아이디
